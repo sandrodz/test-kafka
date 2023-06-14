d885b6c9b27a0e16762257735ccea053f1c71e9f
Sandro Dzneladze <sandrodz@gmail.com>
2023-06-14 23:49:09 +0200

    style: use space for shell scripts

--- debezium.sh
+++ debezium.sh
@@ -3,38 +3,38 @@
 # Function to handle 'up' option
 function handle_up {
   echo "Running 'up' operation..."
-	docker-compose -f docker-compose.yml -f docker-compose.debezium.yml up
+  docker-compose -f docker-compose.yml -f docker-compose.debezium.yml up
 }
 
 # Function to handle 'down' option
 function handle_down {
   echo "Running 'down' operation..."
-	docker-compose -f docker-compose.yml -f docker-compose.debezium.yml down
+  docker-compose -f docker-compose.yml -f docker-compose.debezium.yml down
 }
 
 # Function to handle 'create-connector' option
 function handle_create_connector {
   echo "Running 'create-connector' operation..."
 
-	# https://debezium.io/documentation/reference/stable/connectors/mysql.html#mysql-connector-properties
-	curl -i -X POST -H  "Content-Type:application/json" \
-		http://localhost:8083/connectors \
-		-d '{
-					"name": "mysql-debezium",
-					"config": {
-						"connector.class": "io.debezium.connector.mysql.MySqlConnector",
-						"database.hostname": "mysql",
-						"database.port": "3306",
-						"database.user": "debezium",
-						"database.password": "dbz",
-						"database.server.id": "9999",
-						"database.server.name": "dbserver1",
-						"database.include.list": "demo",
-						"topic.prefix": "db",
-						"database.history.kafka.bootstrap.servers": "kafka1:29092,kafka2:29092,kafka3:29092",
-						"database.history.kafka.topic": "dbhistory.demo"
-					}
-		}'
+  # https://debezium.io/documentation/reference/stable/connectors/mysql.html#mysql-connector-properties
+  curl -i -X POST -H  "Content-Type:application/json" \
+    http://localhost:8083/connectors \
+    -d '{
+      "name": "mysql-debezium",
+      "config": {
+      "connector.class": "io.debezium.connector.mysql.MySqlConnector",
+      "database.hostname": "mysql",
+      "database.port": "3306",
+      "database.user": "debezium",
+      "database.password": "dbz",
+      "database.server.id": "9999",
+      "database.server.name": "dbserver1",
+      "database.include.list": "demo",
+      "topic.prefix": "db",
+      "database.history.kafka.bootstrap.servers": "kafka1:29092,kafka2:29092,kafka3:29092",
+      "database.history.kafka.topic": "dbhistory.demo"
+    }
+  }'
 }
 
 # Function to display help
